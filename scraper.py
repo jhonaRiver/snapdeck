24e5681a55828a3b878168263c36ffb9cae2c6b2
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2024-01-22 17:10:01 -0400

    EOD: Remove lines not used

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from requests_html import HTMLSession, AsyncHTMLSession
+from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 
 
@@ -31,7 +31,6 @@ soup = BeautifulSoup(response.html.html, 'html.parser')
 
 # Find the HTML elements that contain card information
 card_list = soup.find_all('a', class_='simple-card')
-print(len(card_list))
 
 # List to store card instances
 cards = []
@@ -45,6 +44,7 @@ for card in card_list:
         card_ability = card.get('data-ability')
         cards.append(Card(card_name, card_tag,
                      card_ability, card_cost, card_power))
-print(len(cards))
 
 session.close()
+
+# TODO: Add card information to a database
